442869f0608cb7d177956eae90bd676e4c26a507
tetianahrebeniuk <taniahrebeniuk@gmail.com>
2021-11-25 19:24:47 +0200

    homework6

--- src/homework6/GenerateRandomNumbers.java
+++ src/homework6/GenerateRandomNumbers.java
@@ -1,9 +1,5 @@
 package homework6;
 
-/* Write a method that accepts two parameters: mode of type String and length of type int.
-This function should return a randomly generated string of given length according to the specified mode.
-Modes: alpha, numeric, alphanumeric
-Output —> String. */
 
 public class GenerateRandomNumbers {
 
